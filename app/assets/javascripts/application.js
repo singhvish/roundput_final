660ae6610b64253d35505dd99cf607822e7251fe
Vishwajit Singh <vishwajit7@gmail.com>
2018-10-17 21:31:11 +0530

    static assets set to true

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 
+//= require rails-ujs
 //= require turbolinks
 = require_tree .
-//= require rails-ujs
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Roundputfinal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
