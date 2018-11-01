69f5b8651127c6bcb003f92c95be7118dd9a9b58
Vishwajit Singh <vishwajit7@gmail.com>
2018-11-01 21:14:18 +0530

    Added redirect to current page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+  	if params[:redirect_to].present?
+  		store_location_for(resource, params[:redirect_to])
+  	elsif request.referer == new_session_url
+  		super
+  	else
+  		stored_location_for(resource) || request.referer || root_path
+  	end
+end
+
+
   protected
 
   def configure_permitted_parameters
